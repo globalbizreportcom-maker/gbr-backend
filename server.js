9863f58859da150db1c6cd9e0b5437396d88bfa4
pabishek61001 <pabishek61001@gmail.com>
2025-12-08 14:58:13 +0530

    server rebuild

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.disable("x-powered-by");
 
 // Middleware
 const allowedOrigins = [
-    // "http://localhost:3000",// --dev
+    // "http://localhost:3000",// --devv
     /\.globalbizreport\.com$/,
     "https://globalbizreport.com",
     "https://www.globalbizreport.com",
